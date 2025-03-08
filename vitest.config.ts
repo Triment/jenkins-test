411916627c4f272cd1a7ac51e0477628c25cd3bb
as <as@asdeiMac.local>
2025-03-08 16:37:03 +0800

    test

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      name: 'chromium',
+      enabled: true,
+      provider: 'playwright',
+      instances: [
+        { browser: 'chromium' },
+      ],
+    },
+  },
+})
